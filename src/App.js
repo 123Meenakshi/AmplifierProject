8c82d6ee5cfcbf4a5b1195f3fa20666d412c94aa
123Meenakshi <101257969+123Meenakshi@users.noreply.github.com>
2022-05-12 11:46:18 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,54 @@
-const num1 = 5;
-const num2 = 3;
+import './App.css';
+import React,{useState} from 'react';
 
-// add two numbers
-const sum = num1 + num2;
+function App() {
+  const [currentInterest,setCurrentSum]=useState(0);
+    const [clear,setClear]=useState(false);
+    const Calculate=(e)=>{
+      e.preventDefault();
+      if(clear) setClear(false);
+      let pa=document.querySelector('#p').value
+      let r=document.querySelector('#r').value
+      let t=document.querySelector('#t').value
+      
+      let interest = (pa*t*r)/100;
+      alert("Simple Interest = "+interest);
+      //document.querySelector('#num').value=interest;
+        
+    } 
 
-// display the sum
-console.log('The sum of ' + num1 + ' and ' + num2 + ' is: ' + sum);
+    const Clear=(e)=>{
+      e.preventDefault();
+      document.querySelector('form').reset();
+      setClear(true);
+    }
+
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+        <p>
+          Simple Interest Calculator
+        </p>
+
+        <form>
+            <label>Principle Amount:</label>
+            <input className="form-control" type="number" id="p"  /> <br></br>
+
+            <label>Rate of Interest:</label>
+            <input className="form-control" type="number" id="r"  /> <br></br>
+
+            <label>Time(Y):</label>
+            <input class="form-control" type="number" id="t"  /> <br></br>
+
+            <button onClick={Calculate}>Calculate</button>
+            <button onClick={Clear}>Clear</button>
+        </form>
+       
+      </header>
+    </div>
+  );
+}
+
+export default App;
